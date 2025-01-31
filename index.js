612b290541592957afa9823d614ead8981210b35
anikush-yes <anikush@hotmail.com>
2025-01-31 10:06:11 +0200

    added 4 files to .
    Signed-off-by: anikush-yes <anikush@hotmail.com>

---
+++ index.js
@@ -0,0 +1,33 @@
+// console.log('test');
+
+
+const express = require('express'); //require pasiima is modules express
+const app = express();
+
+//prisijungimas prie duombazes
+
+app.use(express.json());//requestams ir responsams
+
+//         Apsirasyti ROUTES - kelias:
+
+// GET        /products - route 
+// GET         /products/:id - route 
+// POST       /products/create - route
+// PUT/PATCH /products/update/:id - route 
+// DELETE   /products/delete/:id - route
+
+//req - request
+//res - response
+//localhost:3000/products
+//{message: 'Sėkmingai pasiekiamas produktų puslapis'} status kodas 200
+
+app.get('/products', async (req,res) => {
+    //neapibrezta klaida
+    res.status(200).json({message: 'Sėkmingai pasiekiamas produktų puslapis'})
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`) //paleidziamas serveris
+});
