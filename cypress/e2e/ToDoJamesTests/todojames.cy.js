2a2d18ab25e9995223494d38dca7af57c4781c32
anikush-yes <anikush@hotmail.com>
2025-02-12 11:45:09 +0200

    modified todojames.cy.js in ToDoJamesTests
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- cypress/e2e/ToDoJamesTests/todojames.cy.js
+++ cypress/e2e/ToDoJamesTests/todojames.cy.js
@@ -11,8 +11,45 @@ it('Create new to do', () => {
 
 
     cy.get('input.new-todo').type('1 uzduotis{enter}');
-//patikrinti ar pirma uzduotis atsidare uzduociu sarase
+//patikrinti ar pirma uzduotis atsirado uzduociu sarase
+
+cy.get('ul.todo-list li').should('contain', '1 uzduotis');
 
 });
 
 
+it('Delete new to do', () => {
+    cy.visit('https://todolist.james.am/#/');
+    cy.get('input.new-todo').type('1 uzduotis{enter}')
+    cy.get('input.new-todo').type('1 uzduotis trinimui{enter}');
+   
+    cy.contains('ul.todo-list li', '1 uzduotis trinimui') .find('button.destroy').invoke('show');
+    cy.contains('ul.todo-list li', '1 uzduotis trinimui') .find('button.destroy').click();
+
+    
+cy.contains('ul.todo-list li', '1 uzduotis trinimui').should('not.exist');
+
+
+
+});
+
+
+it('To do item edit', () =>{
+    cy.visit('https://todolist.james.am/#/');
+    cy.get('input.new-todo').type('2 uzduotis{enter}');
+    cy.get('input.new-todo').type('3 uzduotis{enter}');
+    cy.get('input.new-todo').type('4 uzduotis{enter}');
+
+
+
+
+cy.contains('ul.todo-list li', '2 uzduotis')
+.dblclick()
+.find('input.edit')
+.clear()
+.type('redaguota uzduotis{enter}');
+
+cy.contains('ul.todo-list li', 'redaguota uzduotis').should('be.visible');
+
+
+});
