10390349b51132ecf71f8ee7d02cda9e6ce01f9a
anikush-yes <anikush@hotmail.com>
2025-02-20 08:56:27 +0200

    modified intercept.cy.js in intercept-tests
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- cypress/e2e/intercept-tests/intercept.cy.js
+++ cypress/e2e/intercept-tests/intercept.cy.js
@@ -1,30 +1,24 @@
 /// <reference types="cypress" />
 
-
-it('Request, intercept, visit', () => {
-
-//API
-cy.request("GET", "https://jsonplaceholder.typicode.com/posts/1").then((response) => {
-    // expect(response.status).to.be.eq(200); //ar status kodas 200
-});
-
-//Internetinė svetainė
-cy.visit('https://todolist.james.am/#/') // tiesiog uzeinu i svetaine
-
-//perimti
-//sis metodas dazniau naudojamas API
-cy.intercept("GET", "https://jsonplaceholder.typicode.com/posts/104");
-
-});
+///Intercept paaiskinimas
 
 
-
-
-it('Get a post/mock a post', () =>{
+it('Get a post/mock post', () => {
+    //gauti originalų post kurio id = 1
     cy.request("GET", "https://jsonplaceholder.typicode.com/posts/1").then((response) => {
-       cy.log(response.body)
+        cy.log(response.body);
     });
 
-    cy.request("GET", "https://jsonplaceholder.typicode.com/posts/1", {body: {userId: 104, title:'perimtas pavadinimas, id:104'}}).as('getPostMock')
-cy.wait('@getPostMock');
+    // cy.intercept, nereaguoja į cy.request
+    // cy.intercept reaguoja tik į cy.visit
+
+    //Sumanipuliuoti, periimti post kurio id = 1
+    //{userId: 104, title: 'perimtas pavadinimas', id: 104}
+    cy.intercept('GET', "https://jsonplaceholder.typicode.com/todos/1", {
+        ststusCode: 201,
+        body: { userId: 104, title: 'perimtas pavadinimas', id: 104 }
+    }).as('getPostMock');
+    cy.visit('https://jsonplaceholder.typicode.com');
+    cy.get('#run-button').click();
+    cy.wait('@getPostMock');
 });
